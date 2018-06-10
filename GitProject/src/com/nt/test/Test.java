eaecd566ffb847fe0fbd3f03181dee9fdef93aa0
Rajesh <Rajesh@DESKTOP-GKB890B>
2018-06-10 18:06:48 +0530

    changes made by dev2

--- GitProject/src/com/nt/test/Test.java
+++ GitProject/src/com/nt/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-System.out.println("hi");
+
 System.out.println("welcome to Hydrabad");
 System.out.println("what about hydrabad");
 	}
