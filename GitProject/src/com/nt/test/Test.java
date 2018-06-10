c574ccdbefbfe7d2034989de49f83b06f19db9f0
Rajesh <Rajesh@DESKTOP-GKB890B>
2018-06-10 18:40:27 +0530

    midified by tl

--- GitProject/src/com/nt/test/Test.java
+++ GitProject/src/com/nt/test/Test.java
@@ -1,5 +1,7 @@
 package com.nt.test;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,6 +9,10 @@ public class Test {
 
 System.out.println("welcome to Hydrabad");
 System.out.println("what about hydrabad");
+System.out.println("the output is"+1+2);
+System.out.println("hello welcom to all");
+System.out.println("good afternoon");
 	}
+	
 
 }
